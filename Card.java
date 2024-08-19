f1ec74225f5f98964b6820c868fa00a61ce7f4fa
matt <mattcompton@gmail.com>
2024-08-18 18:32:21 -0700

    new update

--- Card.java
+++ Card.java
@@ -1,12 +1,33 @@
 public class Card {
     private String face;
     private String suit;
+    private int value;
 
-    public Card(String face, String suit){
+    public Card(String face, String suit, int value){
         this.face = face;
         this.suit = suit;
+        this.value = value;
+        System.out.println("!!");
     }
 
+    public String getFace(){
+        return face;
+    }
+    public String getSuit(){
+        return suit;
+    }
+    public int getValue(){
+        return value;
+    }
 
+    public void setFace(String face){
+        this.face = face;
+    }
+    public void setSuit(String suit){
+        this.suit = suit;
+    }
+    public void setValue(int value){
+        this.value = value;
+    }
 
 }
--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
         //System.out.println(newMenu.selection);
         // Create an instance of a new game
         Game game = new Game(newMenu.selection,5000);
-        game.LaunchGame();
+        //game.LaunchGame();
+
     }
 }
